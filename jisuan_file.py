e3ea558049a5b70617d7677616d3b6ea58bc5614
wfk <wfk0924@163.com>
2019-12-23 10:19:21 +0800

    del

--- jisuan_file.py
+++ jisuan_file.py
@@ -6,6 +6,10 @@ from data_to_heavy import get_file
 
 
 if __name__ == '__main__':
-    path = '../JY_data/RenBao_huan_xiu/RenBao_huan_xiu_20190708'
+    path = '../JY_data/segment/glass/2019-12-13-singapore_glass_data'
     file_list = get_file(path)
-    print(len(file_list))
+    list = []
+    for i in file_list:
+        if i.endswith('.JPG'):
+            list.append(i)
+    print(len(list))
